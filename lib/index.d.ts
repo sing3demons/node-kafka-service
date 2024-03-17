6e0ca127f32e4b822379e098bc653679f13cd72c
sing3demons <sing3demons@live.com>
2024-03-17 08:51:49 +0700

    Refactor KafkaService class and add JSDoc comments

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,9 +4,39 @@ export declare class KafkaService {
     private kafka;
     private logger;
     private admin;
+    /**
+     * Initializes the KafkaService.
+     * env variables:
+     * KAFKA_BROKERS: comma separated list of kafka brokers
+     * KAFKA_CLIENT_ID: client id for kafka
+     * KAFKA_REQUEST_TIMEOUT: request timeout for kafka
+     * KAFKA_RETRY: number of retries for kafka
+     * KAFKA_INITIAL_RETRY_TIME: initial retry time for kafka
+     * KAFKA_LOG_LEVEL: log level for kafka
+     */
     constructor();
+    /**
+     * Creates a Kafka topic.
+     *
+     * @param topic - The name of the topic to create. :: ex => topic=topic1,topic2
+     * @returns A promise that resolves when the topic is created.
+     */
     createTopic(topic: string): Promise<void>;
+    /**
+     * Sends a message to a Kafka topic.
+     *
+     * @param topic - The name of the Kafka topic.
+     * @param message - The message to be sent. It can be an array of objects, a single object, or a string.
+     * @param headers - Optional headers to be included in the message.
+     * @returns A Promise that resolves to the record of the sent message.
+     */
     sendMessage(topic: string, message: Array<Object> | Object | string, headers?: IHeaders): Promise<import("kafkajs").RecordMetadata[]>;
+    /**
+     * Consume messages from a Kafka topic and invoke the provided callback for each message.
+     * @param topic - The Kafka topic to consume messages from.
+     * @param callback - The callback function to be invoked for each consumed message.
+     * @returns A Promise that resolves when the consumption is complete.
+     */
     consumeMessages(topic: string, callback: MessageCallback): Promise<void>;
 }
 export {};
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,17 @@ export class KafkaService {
   private logger: Logger
   private admin: Admin
 
+  
+  /**
+   * Initializes the KafkaService.
+   * env variables:
+   * KAFKA_BROKERS: comma separated list of kafka brokers
+   * KAFKA_CLIENT_ID: client id for kafka
+   * KAFKA_REQUEST_TIMEOUT: request timeout for kafka
+   * KAFKA_RETRY: number of retries for kafka
+   * KAFKA_INITIAL_RETRY_TIME: initial retry time for kafka
+   * KAFKA_LOG_LEVEL: log level for kafka
+   */
   constructor() {
     const brokers = process.env.KAFKA_BROKERS?.split(',') ?? ['localhost:9092']
     const clientId = process.env.KAFKA_CLIENT_ID ?? 'my-app'
@@ -36,6 +47,13 @@ export class KafkaService {
     })
   }
 
+  
+  /**
+   * Creates a Kafka topic.
+   * 
+   * @param topic - The name of the topic to create. :: ex => topic=topic1,topic2
+   * @returns A promise that resolves when the topic is created.
+   */
   async createTopic(topic: string) {
     const topics: ITopicConfig[] = topic.split(',').map((topic) => ({ topic }))
 
@@ -47,6 +65,14 @@ export class KafkaService {
     await this.admin.disconnect()
   }
 
+  /**
+   * Sends a message to a Kafka topic.
+   * 
+   * @param topic - The name of the Kafka topic.
+   * @param message - The message to be sent. It can be an array of objects, a single object, or a string.
+   * @param headers - Optional headers to be included in the message.
+   * @returns A Promise that resolves to the record of the sent message.
+   */
   async sendMessage(topic: string, message: Array<Object> | Object | string, headers?: IHeaders) {
     if (!headers) {
       headers = {}
@@ -73,6 +99,12 @@ export class KafkaService {
     return record
   }
 
+  /**
+   * Consume messages from a Kafka topic and invoke the provided callback for each message.
+   * @param topic - The Kafka topic to consume messages from.
+   * @param callback - The callback function to be invoked for each consumed message.
+   * @returns A Promise that resolves when the consumption is complete.
+   */
   async consumeMessages(topic: string, callback: MessageCallback) {
     const consumer = this.kafka.consumer({ groupId: 'test-group' })
     await consumer.connect()
